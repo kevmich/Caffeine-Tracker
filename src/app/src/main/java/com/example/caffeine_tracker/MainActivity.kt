630d96a75938fc5ac2e38565fc9ae472ef4d8353
mike_levytskyi <mikhailo.levitskiy@selu.edu>
2021-11-09 21:33:30 -0600

    -Beginning of the design (beginner manipulation)

--- src/app/src/main/java/com/example/caffeine_tracker/MainActivity.kt
+++ src/app/src/main/java/com/example/caffeine_tracker/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.caffeine_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
     }
 }
