a57febb7a58b2fcaeb81f718dc31007fffcba487
mike_levytskyi <mikhailo.levitskiy@selu.edu>
2021-11-14 00:15:10 -0600

    Chart Implemented

--- src/app/src/main/java/com/example/caffeine_tracker/MainActivity.kt
+++ src/app/src/main/java/com/example/caffeine_tracker/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.example.caffeine_tracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    /** Called when the user taps the Send button */
-    fun sendMessage(view: View) {
-        // Do something in response to button
+        //--------------------------------------------
+        val aaChartView = findViewById<AAChartView>(R.id.aa_chart_view)
+
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Area)
+            .title("title")
+            .subtitle("subtitle")
+            .backgroundColor("#4b2b7f")
+            .dataLabelsEnabled(true)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Tokyo")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+                AASeriesElement()
+                    .name("NewYork")
+                    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
+                AASeriesElement()
+                    .name("London")
+                    .data(arrayOf(0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0)),
+                AASeriesElement()
+                    .name("Berlin")
+                    .data(arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8))
+                )
+            )
+
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
+        //---------------------------------------
     }
 }
