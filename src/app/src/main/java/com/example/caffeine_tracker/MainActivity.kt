193e24383d10811c212a5ddeae1599e66bdd6262
mike_levytskyi <mikhailo.levitskiy@selu.edu>
2021-11-15 13:58:46 -0600

    -minor layout changes

--- src/app/src/main/java/com/example/caffeine_tracker/MainActivity.kt
+++ src/app/src/main/java/com/example/caffeine_tracker/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         /**
          * Set up custom action bar title
          */
